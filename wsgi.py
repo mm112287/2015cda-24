d9963970f9aade033f10b962a088f2ddc8e277f1
40223124 <40223124@gm.nfu.edu.tw>
2015-06-22 22:31:00 +0800

    3&4

--- wsgi.py
+++ wsgi.py
@@ -134,6 +134,22 @@ class Hello(object):
             outstring+=''' <option value="'''+str(i)+'''">'''+str(i)+'''</option>'''
         outstring+='''
         </select><br />
+
+        齒數3:<br />
+        <select name="ng3">
+        '''
+        for a in range(15,86):
+            outstring+=''' <option value="'''+str(a)+'''">'''+str(a)+'''</option>'''
+        outstring+='''
+        </select><br />
+
+        齒數3:<br />
+        <select name="ng4">
+        '''
+        for b in range(15,86):
+            outstring+=''' <option value="'''+str(b)+'''">'''+str(b)+'''</option>'''
+        outstring+='''
+        </select><br />    
         模數:<br />
         <input type=\"text\" name=\"m\"><br />
         壓力角(>33時會有錯誤):<br />
@@ -161,10 +177,15 @@ class Hello(object):
         return outstring
     @cherrypy.expose
     # N 為齒數, M 為模數, P 為壓力角
-    def geartest2(self, m=None, ng1=None, ng2=None, inp2=None):
+    def geartest2(self, m=None, ng1=None, ng2=None, ng3=None, ng4=None, inp2=None):
         outString = ""
         outString +="藍色，24號丞宗所繪製，齒數1:"+ng1
-
+        outString += "<br />"
+        outString +="齒數2:"+ng2
+        outString += "<br />"
+        outString +="齒數3:"+ng3
+        outString += "<br />"
+        outString +="齒數4:"+ng4
         outString += "<br />"
         outString +="模數:"+m
         outString += "<br />"
@@ -209,13 +230,15 @@ class Hello(object):
     # 齒輪齒數
     n_g1 = '''+str(ng1)+'''
     n_g2 = '''+str(ng2)+'''
-
+    n_g3 = '''+str(ng3)+'''
+    n_g4 = '''+str(ng4)+'''
 
 
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
-
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
 
     # 繪圖齒輪的圓心座標,假設排列成水平, 表示各齒輪圓心 y 座標相同
     x_g1 = 400
@@ -224,6 +247,14 @@ class Hello(object):
     x_g2 = x_g1 
     y_g2 = y_g1+ rp_g1 + rp_g2
 
+    x_g3 = x_g2 + rp_g2+ rp_g3
+    y_g3 = y_g2
+
+    x_g3 = x_g2 + rp_g2+ rp_g3
+    y_g3 = y_g2
+
+    x_g4 = x_g3 
+    y_g4 = y_g3+ rp_g3+ rp_g4
 
     #齒輪嚙合的旋轉角
     # 將第1齒輪順時鐘轉 180度
@@ -231,6 +262,8 @@ class Hello(object):
 
     # 將第2齒輪再多轉一齒, 以便與第1齒輪進行囓合
     th2 = -pi/n_g2
+    th3 = -pi/2-pi/n_g3+(pi/2+pi/n_g2)*n_g2/n_g3
+    th4 = -pi/n_g4+(-pi/2+pi/n_g3)*n_g3/n_g4-(pi/2+pi/n_g2)*n_g2/n_g4
 
 
 
@@ -246,6 +279,7 @@ class Hello(object):
     ctx.font = "10px Verdana";
     ctx.fillText("組員:24號袁丞宗所繪製",x_g1-60, y_g1-10);
 
+    #2
     ctx.save()
     # translate to the origin of second gear
     ctx.translate(x_g2,y_g2)
@@ -256,8 +290,30 @@ class Hello(object):
     spur.Spur(ctx).Gear(x_g2,y_g2,rp_g2,n_g2, pa, "blue")
     ctx.restore()
 
+    #3
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g3,y_g3)
+    # rotate to engage
+    ctx.rotate(th3)
+    # put it back
+    ctx.translate(-x_g3,-y_g3)
+    spur.Spur(ctx).Gear(x_g3,y_g3,rp_g3,n_g3, pa, "black")
+    ctx.restore()
+
+    #4
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4,y_g4)
+    # rotate to engage
+    ctx.rotate(th4)
+    # put it back
+    ctx.translate(-x_g4,-y_g4)
+    spur.Spur(ctx).Gear(x_g4,y_g4,rp_g4,n_g4, pa, "black")
+    ctx.restore()
+
     </script>
-    <canvas id="plotarea" width="2800" height="1200"></canvas>
+    <canvas id="plotarea" width="2800" height="2800"></canvas>
     </body>
     </html>
     '''
